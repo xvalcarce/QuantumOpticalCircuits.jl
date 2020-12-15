380b7ede82f192c29c8853e2ba1221f7c6ad6bc0
Xavier Valcarce <xvalcarce@protonmail.ch>
2020-12-15 11:53:08 +0100

    Corrected full connectivity gaussian

--- src/opticaldevices_gaussian.jl
+++ src/opticaldevices_gaussian.jl
@@ -1,4 +1,4 @@
-import SparseArrays: sparse
+import SparseArrays: SparseMatrixCSC, sparse
 
 function ps(ϕ::Float64)
 	r = [cos(ϕ) sin(ϕ)
@@ -63,6 +63,13 @@ function bs(θ::Float64)
 	return r
 end
 
+function swap()
+	r = sparse([4,3,2,1],
+			[1,2,3,4],
+			[1,-1,1,-1.])
+	return r
+end
+
 function apply(od::OpticalDevice,state::GaussianState)
 	if od.optdev ∈ [disp,disp_re,disp_im]
 		d = od.optdev(od.param,state.d,od.mode)	
@@ -104,15 +111,23 @@ function apply!(od::OpticalDevice,state::GaussianState)
 		if typeof(od.mode) == Int
 			r[2od.mode-1:2od.mode,2od.mode-1:2od.mode] = r_
 		elseif length(od.mode) == 2
-			if od.mode[2]-od.mode[1]==1
+			Δmode = abs(od.mode[2]-od.mode[1])
+			if Δmode == 1
 				r[2od.mode[1]-1:2od.mode[2],2od.mode[1]-1:2od.mode[2]] = r_
-			else
+			else 
 				# Ensuring full connectivity
-				for (idx,i) in enumerate(od.mode)
-					for (jdx,j) in enumerate(od.mode)
-						r[2i-1:2i,2j-1:2j] = r_[2idx-1:2idx,2jdx-1:2jdx]
-					end
+				# TODO: possible speed up swap mode (n,m) directly?
+				swaps = Vector{SparseMatrixCSC{Float64,Int64}}()
+				swap_ = swap()
+				dom,upm = od.mode[2]>od.mode[1] ? od.mode : sort(od.mode) 
+				for m in 1:Δmode-1
+					m_ = upm-m
+					s = sparse(dg,dg,1.0)
+					s[2m_-1:2(m_+1),2m_-1:2(m_+1)] = swap_
+					push!(swaps,s)
 				end
+				r[2dom-1:2(dom+1),2dom-1:2(dom+1)] = r_
+				r = *(swaps...,r,reverse(swaps)...)
 			end
 		else
 			throw(">2 modes operations are not implemented")
