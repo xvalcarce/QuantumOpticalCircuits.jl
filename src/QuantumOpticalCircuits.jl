798c42e8b35dd5d3d31ea97ccc7449a0415676fb
Xavier Valcarce <xvalcarce@protonmail.ch>
2022-04-06 19:19:22 +0200

    Clean up legacy files

--- src/QuantumOpticalCircuits.jl
+++ src/QuantumOpticalCircuits.jl
@@ -1,23 +1,7 @@
 module QuantumOpticalCircuits
 
-export State,
-	GaussianState, vacuum_gaussian,
-	PseudoGaussianState, vacuum_pseudogaussian,
-	FockState, vacuum_fock,
-	OpticalDevice, apply, apply!,
-	PS, SMS, SMS_Re, SMS_Im, D, D_Re, D_Im, TMS, BS,
-	Measure, correlator, 
-	PhotonDetector, Heralding,
-	Circuit, FockCircuit, GaussianCircuit, PseudoGaussianCircuit,
-	add_gate!, compute, compute!, print
-
 include("states/states.jl")
-
-abstract type Gate end
-
 include("opticaldevices/opticaldevices.jl")
 include("measures/measures.jl")
-
-include("circuits.jl")
 
 end
