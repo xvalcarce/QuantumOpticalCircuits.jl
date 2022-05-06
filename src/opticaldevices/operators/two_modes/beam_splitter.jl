c0df8a5ae2ae7dc543d17b540bed1603f3b9303f
Xavier Valcarce <xvalcarce@protonmail.ch>
2022-05-06 12:07:03 +0200

    Correct BeamSplitter

--- src/opticaldevices/operators/two_modes/beam_splitter.jl
+++ src/opticaldevices/operators/two_modes/beam_splitter.jl
@@ -20,9 +20,9 @@ BS(θ::Real) = BeamSplitter(θ)
 function mat(bs::BeamSplitter)
 	cθ = cos(bs.θ)
 	sθ = sin(bs.θ)
-	r = sparse([1,2,3,4,4,3,2,1],
-		[1,2,3,4,1,2,3,4],
-		[cθ,cθ,cθ,cθ,sθ,-sθ,sθ,-sθ])
+	r = sparse([1,2,3,4,1,2,3,4],
+		[1,2,3,4,3,4,1,2],
+		[cθ,cθ,cθ,cθ,sθ,sθ,-sθ,-sθ])
 	return r
 end
 
